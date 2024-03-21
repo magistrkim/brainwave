02e5dcb30f5fab65be70e7f6d2024c021f5d3a19
Nataliya Kim <magistr1982@gmail.com>
2024-03-21 12:44:56 +0200

    Create Benefits component

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,3 +1,4 @@
+import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
 
@@ -9,6 +10,21 @@ const Benefits = () => {
           className="md:max-w-md lg:max-w-2xl"
           title="Chat Smarter, Not Harder with Brainwave"
         />
+        <div className="flex flex-wrap gap-10 mb-10">
+          {benefits.map((item) => (
+            <div
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              key={item.id}
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+              }}
+            >
+              <div>
+                <h5>{item.title}</h5>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </Section>
   );
