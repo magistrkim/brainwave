5d76292d3d27d43f289f40667495e013149b1743
Nataliya Kim <magistr1982@gmail.com>
2024-03-22 10:25:38 +0200

    Add Collaboration markup

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,9 +1,23 @@
+import { collabContent } from "../constants";
 import Section from "./Section";
 
 const Collaboration = () => {
   return (
     <Section crosses id="collaboration">
-      <div className="container lg:flex">Collaboration</div>
+      <div className="container lg:flex">
+        <div className="max-w-[25rem]">
+          <h2 className="h2 mb-4 md:mb-8">
+            AI Chat App for seamless collaboration
+          </h2>
+          <ul className="max-w-[22rem] mb-10 md:mb-14">
+            {collabContent.map((item) => (
+              <li key={item.id} className="mb-3 py-3">
+                {item.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </Section>
   );
 };
