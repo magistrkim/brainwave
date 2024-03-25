8f0310e69d76be7d88687c2baf96ad1fdb736b8d
Nataliya Kim <magistr1982@gmail.com>
2024-03-25 11:39:11 +0200

    Create Pricing component

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,11 @@
+import Section from "./Section";
+
+const Pricing = () => {
+  return (
+    <Section>
+      <div className="container">Pricing</div>
+    </Section>
+  );
+};
+
+export default Pricing;
