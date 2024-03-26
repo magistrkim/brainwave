5fddd2ea5bb9dded392328fbb467017496f79200
Nataliya Kim <magistr1982@gmail.com>
2024-03-26 11:55:46 +0200

    Create Tagline component

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,9 +1,34 @@
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
 import Section from "./Section";
 
 const Pricing = () => {
   return (
-    <Section>
-      <div className="container">Pricing</div>
+    <Section className="overflow-hidden" id="pricing">
+      <div className="container relative z-2">
+        <div className="hidden lg:flex justify-center relative mb-[6.5rem]">
+          <img
+            src={smallSphere}
+            alt="sphere"
+            width={255}
+            height={255}
+            className="relative z-1"
+          />
+          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+            <img
+              src={stars}
+              alt="stars"
+              width={950}
+              height={400}
+              className="w-full"
+            />
+          </div>
+        </div>
+        <Heading
+          tag="Get started with Brainwave"
+          title="Pay once, use forever"
+        />
+      </div>
     </Section>
   );
 };
