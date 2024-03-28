231b20e7c9ddd09a17e207b5904535a25af20f5e
Nataliya Kim <magistr1982@gmail.com>
2024-03-28 11:20:06 +0200

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import Section from "./Section";
+
+const Footer = () => {
+  return (
+    <Section>
+      <div className="container">Footer</div>
+    </Section>
+  );
+};
+
+export default Footer;
